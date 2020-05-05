c245839ea25fc2d4f772d4653faf45a99ac116da
2donghoon <eehdgns6114@naver.com>
2020-05-05 20:19:37 +0900

    파이썬 과제

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
 print("hello")
-손창하 = 제 이름은 손창하입니다. "01067662333" = 제 전화번호는 '01067662333'입니다.
-print(손창하, "0106766233")
+a = "제 이름은 손창하입니다."
+b = "제 전화번호는 '01067662333'입니다."
+a = input("제 이름은 손창하입니다.")
+b = input("제 전화번호는 '01067662333'입니다.")
+c = "My  naMe  is  Son  Chang  Ha:  my  pin  is  000125-3!!!!!!."
+d = c.split()
+g = d[1].lower()
+h = d[6].capitalize()
+i = " ".join(c.split())
+e = i.replace("!!!!!!","")
+f = e.replace(":",",")
+j = f.replace("naMe","name")
+k = j.replace("my","My")
+print(k)
