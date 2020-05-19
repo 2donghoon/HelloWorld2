d50c6d72105bb30272d3dd7ca210818d329434a5
2donghoon <eehdgns6114@naver.com>
2020-05-19 20:10:50 +0900

    파이썬 과제

--- hello.py
+++ hello.py
@@ -1,15 +1,19 @@
-print("hello")
-a = "제 이름은 손창하입니다."
-b = "제 전화번호는 '01067662333'입니다."
-a = input("제 이름은 손창하입니다.")
-b = input("제 전화번호는 '01067662333'입니다.")
-c = "My  naMe  is  Son  Chang  Ha:  my  pin  is  000125-3!!!!!!."
-d = c.split()
-g = d[1].lower()
-h = d[6].capitalize()
-i = " ".join(c.split())
-e = i.replace("!!!!!!","")
-f = e.replace(":",",")
-j = f.replace("naMe","name")
-k = j.replace("my","My")
-print(k)
+print("1.더하기\n2.빼기\n3.곱하기\n4.나누기\n5.다시 입력하기\n6.종료")
+
+while True:
+    start = int(input("원하는 계산기 기능을 입력하세요. "))
+    if(start <= 4):
+        numberA = int(input("첫번째 숫자를 입력하세요. "))
+        numberB = int(input("두번째 숫자를 입력하세요. "))
+        if(start == 1):
+            print("결과는 %d 입니다."%(numberA+numberB))
+        elif(start == 2):
+            print("결과는 %d 입니다."%(numberA-numberB))
+        elif(start == 3):
+            print("결과는 %d 입니다."%(numberA*numberB))
+        elif(start == 4):
+            print("결과는 %d 입니다."%(numberA/numberB))
+    elif(start == 5):
+        print("다시 입력하세요.")
+    elif(start == 6):
+        break
