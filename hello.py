0b9dd53da2e9acbde7e564267cd775bb54becda0
2donghoon <eehdgns6114@naver.com>
2020-06-02 00:45:51 +0900

    python

--- hello.py
+++ hello.py
@@ -1,19 +1,50 @@
-print("1.더하기\n2.빼기\n3.곱하기\n4.나누기\n5.다시 입력하기\n6.종료")
+print("1.더하기\n2.빼기\n3.곱하기\n4.나누기")
+
+def sum(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
 
 while True:
-    start = int(input("원하는 계산기 기능을 입력하세요. "))
-    if(start <= 4):
-        numberA = int(input("첫번째 숫자를 입력하세요. "))
-        numberB = int(input("두번째 숫자를 입력하세요. "))
-        if(start == 1):
-            print("결과는 %d 입니다."%(numberA+numberB))
-        elif(start == 2):
-            print("결과는 %d 입니다."%(numberA-numberB))
-        elif(start == 3):
-            print("결과는 %d 입니다."%(numberA*numberB))
-        elif(start == 4):
-            print("결과는 %d 입니다."%(numberA/numberB))
-    elif(start == 5):
-        print("다시 입력하세요.")
-    elif(start == 6):
+    menu = int(input("원하는 계산기 기능을 입력하세요."))
+    if(menu <= 4):
+        numberA = int(input("첫 번째 숫자를 입력하세요."))
+        numberB = int(input("두 번째 숫자를 입력하세요."))
+        if(menu == 1):
+            result = sum(numberA, numberB)
+            print("결과는 %d입니다."%result)
+            file = open("계산기.txt", 'w')
+            file.write("%d")
+            file.close()
+        elif(menu == 2):
+            result = sub(numberA, numberB)
+            print("결과는 %d입니다."%result)
+            file = open("계산기.txt", 'a')
+            file.write("%d")
+            file.close()
+        elif(menu == 3):
+            result = mul(numberA, numberB)
+            print("결과는 %d입니다."%result)
+            file = open("계산기.txt", 'a')
+            file.write("%d")
+            file.close()
+        elif(menu == 4):
+            result = div(numberA, numberB)
+            print("결과는 %d입니다."%result)
+            file = open("계산기.txt", 'a')
+            file.write("%d")
+            file.close()
+    elif(menu == 5):
         break
+    else:
+        print("다시 입력하세요.")
+
+
+
