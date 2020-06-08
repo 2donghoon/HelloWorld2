6f62ee8c826a5ec6960f8b4820dd898ebd75a310
2donghoon <eehdgns6114@naver.com>
2020-06-09 00:04:25 +0900

    python

--- hello.py
+++ hello.py
@@ -1,50 +1,22 @@
-print("1.더하기\n2.빼기\n3.곱하기\n4.나누기")
+class human:
+    name = "name"
+    HP = 100
+    MP = 100
+    print("안녕하세요")
 
-def sum(a,b):
-    return a+b
+class player(human):
+    AD = 70
 
-def sub(a,b):
-    return a-b
+    def att(self,enemy):
+        if self.MP == 100:
+            self.MP = 50
+            enemy.HP = 50
 
-def mul(a,b):
-    return a*b
+class enemy(human):
+    DP = 20
 
-def div(a,b):
-    return a/b
-
-while True:
-    menu = int(input("원하는 계산기 기능을 입력하세요."))
-    if(menu <= 4):
-        numberA = int(input("첫 번째 숫자를 입력하세요."))
-        numberB = int(input("두 번째 숫자를 입력하세요."))
-        if(menu == 1):
-            result = sum(numberA, numberB)
-            print("결과는 %d입니다."%result)
-            file = open("계산기.txt", 'w')
-            file.write("%d")
-            file.close()
-        elif(menu == 2):
-            result = sub(numberA, numberB)
-            print("결과는 %d입니다."%result)
-            file = open("계산기.txt", 'a')
-            file.write("%d")
-            file.close()
-        elif(menu == 3):
-            result = mul(numberA, numberB)
-            print("결과는 %d입니다."%result)
-            file = open("계산기.txt", 'a')
-            file.write("%d")
-            file.close()
-        elif(menu == 4):
-            result = div(numberA, numberB)
-            print("결과는 %d입니다."%result)
-            file = open("계산기.txt", 'a')
-            file.write("%d")
-            file.close()
-    elif(menu == 5):
-        break
-    else:
-        print("다시 입력하세요.")
+class npc(human):
+    pass    
 
 
-
+    
