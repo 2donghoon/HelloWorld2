6f8d7687eb310ccaf8498c5237f75bb7f77b5346
2donghoon <eehdgns6114@naver.com>
2020-06-09 11:28:55 +0900

    pythonn

--- hello.py
+++ hello.py
@@ -1,24 +1,24 @@
-1 class human: 
-2     name = "name" 
-3     HP = 100 
-4     MP = 100 
-5     print("안녕하세요") 
-6 
+class human: 
+    name = "name" 
+    HP = 100 
+    MP = 100 
+    print("안녕하세요") 
+
  
-7 class player(human): 
-8     AD = 70 
-9 
+class player(human): 
+    AD = 70 
+
  
-10     def att(self,enemy): 
-11         if self.MP == 100: 
-12             self.MP = 50 
-13             enemy.HP = 50 
-14 
+    def att(self,enemy): 
+        if self.MP == 100: 
+            self.MP = 50 
+            enemy.HP = 50 
+
  
-15 class enemy(human): 
-16     DP = 20 
-17 
+class enemy(human): 
+        DP = 20 
+
  
-18 class npc(human): 
-19     pass     
+class npc(human): 
+    pass     
  
